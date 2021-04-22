a3b79af593a33d774a6b0f31988e962892714540
Mariele Ribeiro <mariele_rbr@hotmail.com>
2021-04-22 10:59:01 -0300

    Página: Sign in

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,63 @@
+import {
+  Flex,
+  Input,
+  Button,
+  Stack,
+  FormLabel,
+  FormControl,
+} from "@chakra-ui/react";
+
 export default function Home() {
-  return <h1>Dashgo</h1>;
+  return (
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        w="100%"
+        maxW={360}
+        bg="gray.800"
+        p="8" // para saber as medidas em px *4 em rem /4
+        borderRadius={8} // aqui é em px mesmo
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <FormControl>
+            <FormLabel htmlFor="E-mail">E-mail</FormLabel>
+
+            <Input
+              name="email"
+              id="email"
+              type="email"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: "gray.900",
+              }}
+              size="lg"
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel htmlFor="senha">Senha</FormLabel>
+
+            <Input
+              name="password"
+              id="senha"
+              type="password"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: "gray.900",
+              }}
+              size="lg"
+            />
+          </FormControl>
+        </Stack>
+
+        <Button type="submit" mt="6" colorScheme="pink">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
 }
