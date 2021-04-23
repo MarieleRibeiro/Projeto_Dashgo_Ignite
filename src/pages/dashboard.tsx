e37d13f542a212e23936a930e45bbb51f71b57fc
Mariele Ribeiro <mariele_rbr@hotmail.com>
2021-04-23 10:50:30 -0300

    Página: Dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,65 @@
 import { Header } from "../components/Header";
-import { Flex } from "@chakra-ui/react";
+import dynamic from "next/dynamic";
+import { Box, Flex, SimpleGrid, Text, theme } from "@chakra-ui/react";
 import { Sidebar } from "../components/Sidebar";
+
+const Chart = dynamic(() => import("react-apexcharts"), {
+  // importação dos graficos -> instalação-> yarn add apexcharts react-apexcharts
+  ssr: false, // -> esse Chart so vai ser carregado pelo lado do browser e nunca pelo lado do servidor por isso false
+});
+// esse tipo de importação é utilizado para carregar algum componente de forma dinamica, ou seja "eu carregar um componente so quando o usuario apertar num botão"
+
+const options = {
+  chart: {
+    toolbar: {
+      show: false,
+    },
+    zoom: {
+      enabled: false,
+    },
+    foreColor: theme.colors.gray[500],
+  },
+  grid: {
+    show: false,
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  tooltip: {
+    enabled: false,
+  },
+  xaxis: {
+    type: "datetime",
+    axisBorder: {
+      color: theme.colors.gray[600],
+    },
+    axisTicks: {
+      color: theme.colors.gray[600],
+    },
+    categories: [
+      "2021-03-18T00:00:00.000Z",
+      "2021-03-19T00:00:00.000Z",
+      "2021-03-20T00:00:00.000Z",
+      "2021-03-21T00:00:00.000Z",
+      "2021-03-22T00:00:00.000Z",
+      "2021-03-23T00:00:00.000Z",
+      "2021-03-24T00:00:00.000Z",
+    ],
+  },
+  fill: {
+    opacity: 0.3,
+    type: "gradient",
+    gradient: {
+      shade: "dark",
+      opacityFrom: 0.7,
+      opacityTo: 0.3,
+    },
+  },
+};
+
+const series = [
+  { name: "serie1", data: [31, 120, 10, 28, 61, 18, 109] }, //-> um unico tipo de dado,quantos tipos de dados eu quero dentro do grafico
+];
 
 export default function Dashboard() {
   return (
@@ -9,6 +68,21 @@ export default function Dashboard() {
 
       <Flex w="100%" my="6" maxW={1480} mx="4" px="6">
         <Sidebar />
+
+        <SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex-start">
+          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
+            <Text fontSize="lg" mb="4">
+              Inscritos da semana
+            </Text>
+            <Chart options={options} series={series} type="area" height={160} />
+          </Box>
+          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
+            <Text fontSize="lg" mb="4">
+              Taxa de abertura
+            </Text>
+            <Chart options={options} series={series} type="area" height={160} />
+          </Box>
+        </SimpleGrid>
       </Flex>
     </Flex>
   );
